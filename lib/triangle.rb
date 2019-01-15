7c59c08c7299d57b3638f35246872edcea804ca9
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-15 22:59:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind
-    if @side1 == @side2 && @side2 == @side3
+    if @side1 == @side2 && @side2 == @side3)
       puts :equilateral
     else
       raise TriangleError
